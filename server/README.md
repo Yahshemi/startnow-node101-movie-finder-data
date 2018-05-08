9993430bb3bc8faa3655ff793512ccfefb44c54d
Yahshemi Walters <Yahshemi@gmail.com>
2018-05-07 19:18:27 -0700

    first commit

---
+++ server/README.md
@@ -0,0 +1,5 @@
+# Movie Finder Data
+
+# Getting Started
+
+Use the Express framework to build a server that can talk to another server and cache the results.
---
+++ server/app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const axios = require('axios');
+const morgan = require('morgan');
+
+const app = express();
+var cache = {};
+//log each request using morgan's dev format.
+app.use(morgan("dev"));
+
+//Server should respond to GET requests to /?i=tt3896198 with movie data.
+app.get('/', function (req, res) {
+    let path = req.url;
+    
+    console.log("Path", path);
+
+    if (path === '/') {
+        return res.send("No title or movie id provided!");
+    }
+
+    // if path is the same as before get it from cache^
+    //http://www.omdbapi.com/?i=tt3896198&apikey=8730e0e
+
+    if (cache[path]) {
+        console.log("Getting movie from cache...");
+        return res.json(cache[path]);
+    }
+
+    console.log("Getting movie from OMDB...");    
+    //otherwise go to api
+    axios
+        .get('http://www.omdbapi.com' + path + '&apikey=8730e0e')
+        .then(function (response) {
+            let data = response.data;
+
+            cache[path] = data;
+            res.status(200).json(data);
+        })
+        .catch(function (error) {
+            res.status(200).json(error.message);
+        })
+    });
+
+    // When making calls to the OMDB API make sure to append the '&apikey=8730e0e' parameter
+
+    module.exports = app;
+
